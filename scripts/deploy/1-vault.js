5661cf2b8c923ccff40ea6dd76c2356394696cb5
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-29 16:06:59 +0200

    Deployed MAI3Pool

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,13 +1,14 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0x45f4682B560d4e3B8FF1F1b3A38FDBe775C7177b';
-  const tokenName = 'TOMB-MAI Tomb Crypt';
-  const tokenSymbol = 'rf-TOMB-MAI';
+  const wantAddress = '0xA58F16498c288c357e28EE899873fF2b55D7C437';
+  const tokenName = 'MAI3Pool-f QiDao Crypt';
+  const tokenSymbol = 'rf-MAI3Pool-f';
   const depositFee = 50;
   const tvlCap = ethers.constants.MaxUint256;
+  const options = {gasPrice: 300000000000, gasLimit: 9000000};
 
-  const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap);
+  const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap, options);
 
   await vault.deployed();
   console.log('Vault deployed to:', vault.address);
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,11 +1,12 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xf3327b00Bb53Bb72365E9B168526693ECeA0a1E4';
+  const strategyAddress = '0xb3481031B8d3c06e0703508566aDAdF5556e4DCd';
+  const options = {gasPrice: 300000000000, gasLimit: 9000000};
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
 
-  await vault.initialize(strategyAddress);
+  await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
 }
 
